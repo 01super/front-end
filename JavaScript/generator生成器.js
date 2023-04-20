12070ce5aefb91d64616092b7ad4ab0b0f1b6568
01super <fk1024@outlook.com>
2023-04-20 10:23:01 +0800

    generator

---
+++ JavaScript/generator生成器.js
@@ -0,0 +1,15 @@
+function* add() {
+  const value1 = yield "first";
+  console.log("value1: ", value1); // 22
+  const value2 = yield "second";
+  console.log("value2: ", value2); // 33
+}
+
+let result;
+const gen = add();
+result = gen.next(11);
+console.log("result: ", result); // first
+result = gen.next(22); // -> log value1
+console.log("result: ", result); // second
+result = gen.next(33); // -> log value2
+console.log("result: ", result); // undefined
